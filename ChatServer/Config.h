4148a4c549d9b994dcc82c7d6fea532b606e366f
losi8812 <j2bun@naver.com>
2019-11-03 00:35:44 +0900

    Change config.

--- ChatServer/Config.h
+++ ChatServer/Config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 namespace LOBBY {
-	static const int MAX_SESSION_COUNT = 6;
-	static const int MAX_ROOM_COUNT = 3;
+	static const int MAX_SESSION_COUNT = 300;
+	static const int MAX_ROOM_COUNT = 100;
 	static const int MAX_THREAD_POOL_SIZE = 8;
 	static const int SERVER_PORT = 31500;
 };
@@ -11,5 +11,5 @@ namespace ROOM {
 	static const int MAX_USER_COUNT = 5;
 	static const int MAX_MESSAGE_QUEUE_SIZE = 5;
 	static const int CHAT_RESET_TIME = 60;
-	static const int MAX_CHAT_COUNT_PER_MIN = 3;
+	static const int MAX_CHAT_COUNT_PER_MIN = 30;
 };
