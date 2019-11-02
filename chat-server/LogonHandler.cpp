4726cc5c9b6061adce3c3b39c18f9dbf3e4587d4
losi8812 <j2bun@naver.com>
2019-11-02 19:57:17 +0900

    project diretory clean task.

--- chat-server/LogonHandler.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "LogonHandler.h"
-
-bool LogonHandler::ChangeName (Session * session, const char * pContext)
-{
-	return false;
-}
--- chat-server/LogonHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Handler.h"
-class LogonHandler :
-	public Handler
-{
-public:
-	static bool ChangeName (Session* session, const char* pContext);
-};
-
